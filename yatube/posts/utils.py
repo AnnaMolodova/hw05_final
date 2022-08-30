4bab2cb5e4679681504078e00eb5f25fb5341300
Anna Molodova <hyperclover@gmail.com>
2022-08-30 19:18:28 +0300

    Исправлены недочеты

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -6,5 +6,5 @@ def Create_Page(queryset, request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return {
-        'page_obj': page_obj
+        'page_obj': page_obj  # Без словаря  тесты ломаются
     }
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -29,7 +29,6 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
-    template = "posts/profile.html"
     author = get_object_or_404(User, username=username)
     posts = Post.objects.filter(author_id=author.id)
     user = request.user
@@ -43,11 +42,10 @@ def profile(request, username):
         "following": following,
     }
     context.update(Create_Page(posts, request))
-    return render(request, template, context)
+    return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
-    template = 'posts/post_detail.html'
     post = get_object_or_404(Post, pk=post_id)
     comments = post.comments.all()
     form = CommentForm()
@@ -56,12 +54,11 @@ def post_detail(request, post_id):
         'comments': comments,
         'form': form
     }
-    return render(request, template, context)
+    return render(request, 'posts/post_detail.html', context)
 
 
 @login_required
 def post_create(request):
-    template = 'posts/create_post.html'
     form = PostForm(
         request.POST or None,
         files=request.FILES or None
@@ -73,12 +70,11 @@ def post_create(request):
         return redirect(
             'posts:profile', temp_form.author
         )
-    return render(request, template, {'form': form})
+    return render(request, 'posts/create_post.html', {'form': form})
 
 
 @login_required
 def post_edit(request, post_id):
-    template = 'posts/create_post.html'
     post = get_object_or_404(Post, pk=post_id)
     if post.author != request.user:
         return redirect(
@@ -101,7 +97,7 @@ def post_edit(request, post_id):
     }
     return render(
         request,
-        template,
+        'posts/create_post.html',
         context
     )
 
@@ -120,12 +116,7 @@ def add_comment(request, post_id):
 @login_required
 def follow_index(request):
     posts = Post.objects.filter(author__following__user=request.user)
-    paginator = Paginator(posts, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'page_obj': page_obj
-    }
+    context = Create_Page(posts, request)
     return render(request, 'posts/follow.html', context)
 
 
